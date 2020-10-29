e86f34712e1faa6ce03e526c9a1ff36401a77acb
vahidzee <vahid98zee@gmail.com>
2020-10-29 16:37:03 +0330

    add other rasterisation methods compatibility

--- raster/models/template.py
+++ raster/models/template.py
@@ -7,6 +7,10 @@ class RasterModel(torch.nn.Module, ABC):
         super().__init__()
         self.modes = modes
         self.in_channels = (config["model_params"]["history_num_frames"] + 1) * 2 + 3
+        if config['raster_params']['map_type'] == 'semantic_debug':
+            self.in_channels = 3
+        if config['raster_params']['map_type'] == 'box_debug':
+            self.in_channels = (config["model_params"]["history_num_frames"] + 1) * 2
         self.future_len = config["model_params"]["future_num_frames"] // config["model_params"]["future_step_size"]
         self.num_preds = self.modes * 2 * self.future_len
         self.out_dim = self.num_preds + (self.modes if self.modes != 1 else 0)
